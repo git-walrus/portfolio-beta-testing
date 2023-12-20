08137968ac078ea7736d7f9399eace64a4fd73e9
git-walrus <ajd.glitch@gmail.com>
2023-12-20 12:09:43 +0000

    - Modified App.jsx, Abouot.jsx, Home.jsx to add my information. - Add new image for homepage banner.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Amritjyoti Das",
+  title: "Web & Creative Designer",
+  email: "aj.das@unsafe-gaming.world",
+  gitHub: "git-walrus",
+  instagram: "aj_das",
+  linkedIn: "amritjyotidas",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
